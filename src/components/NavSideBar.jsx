1b11f3a84a7a8c03974ffe51266a78907e2e1052
yungts97 <yungts97@gmail.com>
2021-04-21 08:49:28 +0800

    updated app logo with app name

--- src/components/NavSideBar.jsx
+++ src/components/NavSideBar.jsx
@@ -1,6 +1,5 @@
 import NavSideBarLink from 'components/NavSideBarLink'
 import ThemeSwitch from 'components/ThemeSwitch'
-import logo from 'assets/icon.png'
 
 import {
   ChartBarIcon,
@@ -15,11 +14,10 @@ export default function NavSideBar () {
     <div className="h-screen hidden lg:block my-4 ml-4 shadow-lg relative w-80">
       <div className="bg-white h-full rounded-2xl dark:bg-gray-700">
         <div className="flex items-center justify-center pt-6">
-          <img src={logo} className="h-10 w-10" />
+          <AppLogo className="h-10 w-10" />
           <p className="font-bold text-xl text-gray-500 dark:text-white px-3">
             HealthOnline
           </p>
-          <AppLogo className="h-10 w-10" />
         </div>
         <nav className="mt-6">
           <div>
